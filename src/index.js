825fb68bcefd55ded65545460a909b1e54dbae64
Shambhu Jagtap <71923397+shambhuj12@users.noreply.github.com>
2023-08-28 19:09:21 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const custStyle = { color: "brown", fontSize: "20px" };
+
+ReactDOM.render(
+  <h1 style={custStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
